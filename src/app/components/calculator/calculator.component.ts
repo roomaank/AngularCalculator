73f99d282acd690f37d47eb658becf133324f584
Roman <roman.kushka10@gmail.com>
2021-01-26 16:38:39 +0200

    start create new func

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,9 +1,5 @@
-import * as uuid from 'uuid';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { DIGITS, OPERATIONS } from './calculator.constants';
-import { HistoryService } from 'src/app/services/history.service';
-import { Calc } from 'src/app/models/calculator-data';
+import { BUTTONS } from './calculator.constants';
 
 @Component({
   selector: 'app-calculator',
@@ -12,103 +8,13 @@ import { Calc } from 'src/app/models/calculator-data';
 })
 export class CalculatorComponent implements OnInit {
 
-  historyData: Calc;
-  digits = DIGITS;
-  operators = OPERATIONS;
-
-  calculationPlace = '';
-  resultPlace = '';
-  digitFirst: number;
-  digitSecond: number;
-  operator = '';
-  clipboard:number;
-  activateHistory: boolean;
+  buttons = BUTTONS;
 
   constructor(
-    private router: Router,
-    private historyService: HistoryService
   ) { }
 
   ngOnInit(): void {
   }
 
-  clickDigit(digit) {
-    this.calculationPlace += digit;
-    if (this.calculationPlace.length > 20) {
-      this.calculationPlace = 'Invalid length'
-      return;
-    }
-  }
-
-  clickOperator(operator) {
-    if (this.calculationPlace === '') {
-      return;
-    }
-    this.digitFirst = parseFloat(this.calculationPlace)
-    this.operator = operator;
-    this.calculationPlace += operator;
-
-    if (this.resultPlace) {
-      this.calculationPlace = this.resultPlace;
-      this.resultPlace = ''
-    }
-
-  }
-
-
-  result() {
-    this.digitSecond = parseFloat(this.calculationPlace.split(this.operator)[1])
-    if (this.operator === '×') {
-      this.calculationPlace;
-      this.resultPlace = (this.digitFirst * this.digitSecond).toString();
-    } else if (this.operator === '÷') {
-      this.calculationPlace;
-      this.resultPlace = (this.digitFirst / this.digitSecond).toString();
-    } else if (this.operator === '+') {
-      this.calculationPlace;
-      this.resultPlace = (this.digitFirst + this.digitSecond).toString();
-    } else if (this.operator === '-') {
-      this.calculationPlace;
-      this.resultPlace = (this.digitFirst - this.digitSecond).toString();
-    } else {
-      this.resultPlace = 'Error';
-    }
-
-    this.activateHistory = true;
-    const resultData = {
-      id: uuid.v4(),
-      calculate: this.calculationPlace,
-      result: this.resultPlace
-    }
-    this.historyService.addCalcData(resultData).subscribe((value) => {
-      this.historyData = value;
-    })
-    
-  }
-
-  clearAll() {
-    this.calculationPlace = '';
-    this.resultPlace = '';
-    this.activateHistory = false;
-  }
-
-  subtractSignBeforeDigit(){
-    this.digitFirst = parseFloat(this.calculationPlace)
-    const subtractDigit = this.digitFirst * (-1);
-    this.calculationPlace = ''
-    this.resultPlace = subtractDigit.toString()            
-  }
-
-  percentOfNumber(){
-    this.digitFirst = parseFloat(this.calculationPlace)
-    const percentDigit = this.digitFirst / 100;
-    this.calculationPlace = ''
-    this.resultPlace = percentDigit.toString();    
-
-  }
-
-  navigateTo() {
-    this.router.navigate(['history'])
-  }
 
 }
--- src/app/components/calculator/calculator.constants.ts
+++ src/app/components/calculator/calculator.constants.ts
@@ -1,3 +1,78 @@
-export const DIGITS = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
-
-export const OPERATIONS = ['÷', '×', '-', '+'];
+export const BUTTONS = [
+    {
+        type: 'string',
+        value: 'C'
+    },
+    {
+        type: 'string',
+        value: '+/-'
+    },
+    {
+        type: 'string',
+        value: '%'
+    },
+    {
+        type: 'string',
+        value: '÷'
+    },
+    {
+        type: 'number',
+        value: 7
+    },
+    {
+        type: 'number',
+        value: 8
+    },
+    {
+        type: 'number',
+        value: 9
+    },
+    {
+        type: 'string',
+        value: '×'
+    },
+    {
+        type: 'number',
+        value: 4
+    },
+    {
+        type: 'number',
+        value: 5
+    },
+    {
+        type: 'number',
+        value: 6
+    },
+    {
+        type: 'string',
+        value: '-'
+    },
+    {
+        type: 'number',
+        value: 1
+    },
+    {
+        type: 'number',
+        value: 2
+    },
+    {
+        type: 'number',
+        value: 3
+    },
+    {
+        type: 'string',
+        value: '+'
+    },
+    {
+        type: 'number',
+        value: 0
+    },
+    {
+        type: 'string',
+        value: '.'
+    },
+    {
+        type: 'string',
+        value: '='
+    }
+]
